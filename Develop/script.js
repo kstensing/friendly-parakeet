27e0f230ae0e0c510f4bd8d586ce5e7f3341580d
kstensing <stensingk@gmail.com>
2022-01-09 17:20:06 -0600

    added length prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,37 @@
 // Assignment code here
+var generatePassword = function() {
+  
+  //obtain password length min 8 max 129 characters
+  var length = "";
+
+  while (length === "" || length === null) {
+    var characterLength = prompt("How many characters would you like your password to contain?");
+
+    console.log(characterLength);
+    if (isNaN(characterLength)) {
+      window.alert("Password length must be provided as a number.");
+    } else if (characterLength > 129) {
+      window.alert("Password length must be less than 129 characters.");
+    } else if (characterLength === null) {
+      return; 
+    } else if (characterLength < 8) {
+      window.alert("Password length must be at least 8 characters.");
+    } else {
+      length = characterLength;
+      console.log("Your password length is " + length);
+    }
+  }
+  
+
+  //ask about including special characters
+
+
+for ( var i = 0; i < length; i++) {
+  //
+}
+};
+
+// password decisions
 
 
 // Get references to the #generate element
