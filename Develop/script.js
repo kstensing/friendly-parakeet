7765d3ce4ef714b58edce3f188b5d25802a56b15
kstensing <stensingk@gmail.com>
2022-01-09 19:39:00 -0600

    added special char uppercase lowercase and numeric prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,81 @@
 // Assignment code here
+// function to generate a random numeric value
+//var randomNumber = function(min, max) {
+  //var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  //return value;
+//};
+
 var generatePassword = function() {
   
   //obtain password length min 8 max 129 characters
-  var length = "";
+  var range = "";
 
-  while (length === "" || length === null) {
-    var characterLength = prompt("How many characters would you like your password to contain?");
+  while (range === "" || range === null) {
+    var characterRange = prompt("How many characters would you like your password to contain?");
 
-    console.log(characterLength);
-    if (isNaN(characterLength)) {
+    if (isNaN(characterRange)) {
       window.alert("Password length must be provided as a number.");
-    } else if (characterLength > 129) {
+    } else if (characterRange > 129) {
       window.alert("Password length must be less than 129 characters.");
-    } else if (characterLength === null) {
+    } else if (characterRange === null) {
       return; 
-    } else if (characterLength < 8) {
+    } else if (characterRange < 8) {
       window.alert("Password length must be at least 8 characters.");
     } else {
-      length = characterLength;
-      console.log("Your password length is " + length);
+      range = characterRange;
+      console.log("Your password length is " + range);
     }
   }
   
 
   //ask about including special characters
+  var specialChar = window.confirm("Click OK to confirm including special characters.");
 
+    if (specialChar) {
+      console.log("include special characters");
+    } else {
+      console.log("do not include special characters");
+    }
+
+      //ask about including numeric characters
+  var numericChar = window.confirm("Click OK to confirm including numeric characters.");
+
+    if (numericChar) {
+      console.log("include numeric characters");
+   } else {
+      console.log("do not include numeric characters");
+    }
+
+    //ask about including uppercase characters
+  var upperChar = window.confirm("Click OK to confirm including uppercase characters.");
+
+    if (upperChar) {
+      console.log("include uppercase characters");
+    } else {
+      console.log("do not include uppercase characters");
+    }
+
+    //ask about including lowercase characters
+  var lowerChar = window.confirm("Click OK to confirm including lowercase characters.");
+
+  if (lowerChar) {
+    console.log("include lowercase characters");
+  } else {
+    console.log("do not include lowercase characters");
+  }
 
 for ( var i = 0; i < length; i++) {
-  //
+  var randomNumber = Math.floor(Math.random() * letters.length);
+  console.log(randomNumber);
 }
+
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var special = "!#$%&()*+,-./:;<=>?@[]^_`{|}~"
+//var password = "";
 };
+
 
 // password decisions
 
